a6b44f4002dd328f6b09cc0e2ea93c803fa62242
Maximilien Libert <m-motors@freemaxi.fr>
2026-09-17 09:48:49 +0200

    refactor: factorisation de la creation de vehicule (location/vente)

--- src/Controller/AdminVehiculeController.php
+++ src/Controller/AdminVehiculeController.php
@@ -37,71 +37,23 @@ class AdminVehiculeController extends AbstractController
     #[Route('/nouveau-location', name: 'app_admin_vehicule_new_location')]
     public function newLocation(Request $request, EntityManagerInterface $entityManager, VehiculePhotoUploader $photoUploader, ActionLogger $actionLogger): Response
     {
-        $vehicule = new Vehicule();
-        $form = $this->createForm(VehiculeType::class, $vehicule);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $vehicule->setStatut('location');
-
-            $photos = $form->get('photos')->getData();
-            $position = 0;
-
-            $allowedMimeTypes = ['image/jpeg', 'image/png', 'image/webp'];
-            $maxFileSize = 5 * 1024 * 1024; // 5 Mo
-
-            foreach ($photos as $photoFile) {
-                if (!in_array($photoFile->getMimeType(), $allowedMimeTypes, true)) {
-                    $this->addFlash('warning', sprintf('Le fichier "%s" a ete ignore (format non autorise, seuls JPEG/PNG/WEBP sont acceptes).', $photoFile->getClientOriginalName()));
-                    continue;
-                }
-
-                if ($photoFile->getSize() > $maxFileSize) {
-                    $this->addFlash('warning', sprintf('Le fichier "%s" a ete ignore (taille superieure a 5 Mo).', $photoFile->getClientOriginalName()));
-                    continue;
-                }
-
-                $filename = $photoUploader->upload($photoFile);
-
-                $vehiculePhoto = new VehiculePhoto();
-                $vehiculePhoto->setFilename($filename);
-                $vehiculePhoto->setPosition($position);
-                $vehicule->addVehiculePhoto($vehiculePhoto);
-
-                $entityManager->persist($vehiculePhoto);
-
-                $position++;
-            }
-
-            $entityManager->persist($vehicule);
-            $entityManager->flush();
-
-            $actionLogger->log(
-                'creation_vehicule',
-                sprintf('A ajoute le vehicule %s %s (%s) au catalogue location', $vehicule->getMarque(), $vehicule->getModele(), $vehicule->getImmatriculation()),
-                'Vehicule',
-                $vehicule->getId()
-            );
-
-            $this->addFlash('success', 'Vehicule ajoute au catalogue de location.');
-            return $this->redirectToRoute('app_admin_vehicule_index');
-        }
-
-        return $this->render('admin/vehicule/new.html.twig', [
-            'form' => $form,
-            'mode' => 'location',
-        ]);
+        return $this->handleNewVehicule($request, $entityManager, $photoUploader, $actionLogger, 'location');
     }
 
     #[Route('/nouveau-vente', name: 'app_admin_vehicule_new_vente')]
     public function newVente(Request $request, EntityManagerInterface $entityManager, VehiculePhotoUploader $photoUploader, ActionLogger $actionLogger): Response
+    {
+        return $this->handleNewVehicule($request, $entityManager, $photoUploader, $actionLogger, 'vente');
+    }
+
+    private function handleNewVehicule(Request $request, EntityManagerInterface $entityManager, VehiculePhotoUploader $photoUploader, ActionLogger $actionLogger, string $statut): Response
     {
         $vehicule = new Vehicule();
         $form = $this->createForm(VehiculeType::class, $vehicule);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $vehicule->setStatut('vente');
+            $vehicule->setStatut($statut);
 
             $photos = $form->get('photos')->getData();
             $position = 0;
@@ -137,18 +89,18 @@ class AdminVehiculeController extends AbstractController
 
             $actionLogger->log(
                 'creation_vehicule',
-                sprintf('A ajoute le vehicule %s %s (%s) au catalogue vente', $vehicule->getMarque(), $vehicule->getModele(), $vehicule->getImmatriculation()),
+                sprintf('A ajoute le vehicule %s %s (%s) au catalogue %s', $vehicule->getMarque(), $vehicule->getModele(), $vehicule->getImmatriculation(), $statut),
                 'Vehicule',
                 $vehicule->getId()
             );
 
-            $this->addFlash('success', 'Vehicule ajoute au catalogue de vente.');
+            $this->addFlash('success', sprintf('Vehicule ajoute au catalogue de %s.', $statut));
             return $this->redirectToRoute('app_admin_vehicule_index');
         }
 
         return $this->render('admin/vehicule/new.html.twig', [
             'form' => $form,
-            'mode' => 'vente',
+            'mode' => $statut,
         ]);
     }
 
