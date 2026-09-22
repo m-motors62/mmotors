fb71282d4f774c80e570ce6b3308d7cf6d11dfd4
Maximilien Libert <m-motors@freemaxi.fr>
2026-09-22 10:03:40 +0200

    feat: espace client - dashboard et modification du profil, menu deroulant dans le header

---
+++ src/Controller/EspaceClientController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Form\ClientProfileType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/espace-client')]
+#[IsGranted('ROLE_CLIENT')]
+class EspaceClientController extends AbstractController
+{
+    #[Route('', name: 'app_espace_client_dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('espace_client/dashboard.html.twig');
+    }
+
+    #[Route('/profil', name: 'app_espace_client_profile')]
+    public function profile(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        /** @var Client $client */
+        $client = $this->getUser();
+        $contact = $client->getContact();
+
+        $form = $this->createForm(ClientProfileType::class);
+        $form->get('nom')->setData($contact->getNom());
+        $form->get('prenom')->setData($contact->getPrenom());
+        $form->get('telephone')->setData($contact->getTelephone());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact->setNom($form->get('nom')->getData());
+            $contact->setPrenom($form->get('prenom')->getData());
+            $contact->setTelephone($form->get('telephone')->getData());
+
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Vos informations ont ete mises a jour.');
+            return $this->redirectToRoute('app_espace_client_profile');
+        }
+
+        return $this->render('espace_client/profile.html.twig', [
+            'form' => $form,
+        ]);
+    }
+}
